bd67c85ac582bac05f7a1cb0d0f6444e56be8f38
Renuka Fernando <renukapiyumal@gmail.com>
2023-08-05 21:51:36 +0530

    Add sample go docker app

---
+++ go-docker/build-docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+IMAGE_NAME=renukafernando/go-docker:v1
+
+env GOOS=linux GOARCH=amd64 go build -o request-info-amd64 .
+# env GOOS=linux GOARCH=arm64 go build -o request-info-arm64 .
+
+#docker buildx build --push --platform linux/arm64/v8,linux/amd64 -t "$IMAGE_NAME" .
+docker build --build-arg TARGETARCH=amd64 -t "$IMAGE_NAME" .
