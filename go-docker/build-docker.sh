c761a4914b022fe9ebdd362c4e3c950433a34530
Renuka Fernando <renukapiyumal@gmail.com>
2023-08-06 07:36:13 +0530

    Add Rest API Capability to go docker app

--- go-docker/build-docker.sh
+++ go-docker/build-docker.sh
@@ -2,7 +2,8 @@
 
 IMAGE_NAME=renukafernando/go-docker:v1
 
-env GOOS=linux GOARCH=amd64 go build -o request-info-amd64 .
+rm go-app
+env GOOS=linux GOARCH=amd64 go build -o go-app .
 # env GOOS=linux GOARCH=arm64 go build -o request-info-arm64 .
 
 #docker buildx build --push --platform linux/arm64/v8,linux/amd64 -t "$IMAGE_NAME" .
--- go-docker/go.mod
+++ go-docker/go.mod
@@ -1,3 +1,5 @@
 module sample
 
 go 1.19
+
+require github.com/gorilla/mux v1.8.0 // indirect
