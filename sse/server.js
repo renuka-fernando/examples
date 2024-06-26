b29c0e9290d92cffbc1acbd967cab156722c80b3
Renuka Fernando <renukapiyumal@gmail.com>
2024-06-26 15:47:14 +0530

    Add SSE sample app

---
+++ sse/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+// SSE endpoint
+app.get('/events', (req, res) => {
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+    res.flushHeaders();
+
+    // Send a message every 2 seconds
+    const intervalId = setInterval(() => {
+        const now = new Date();
+        res.write(`data: ${now.toLocaleTimeString()}\n\n`);
+    }, 2000);
+
+    // Cleanup when client closes connection
+    req.on('close', () => {
+        clearInterval(intervalId);
+        res.end();
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
